73017af36f58d0bba6994f7e028c5a56a393201d
atahf <ataollah@sabanciuniv.edu>
2024-02-19 15:07:44 +0300

    added file meta data

---
+++ File-Metadata-Microservice/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+require('dotenv').config()
+
+var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+  });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
